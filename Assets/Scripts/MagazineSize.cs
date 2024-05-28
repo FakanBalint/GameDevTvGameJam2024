0c0150bbd49699a312376df3a9bf01e8d7116d77
Hattory2110 <csomorbalint6@gmail.com>
2024-05-29 00:09:23 +0200

    Magazine Singleton

---
+++ Assets/Scripts/MagazineSize.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagazineSize : MonoBehaviour
+{
+
+    [SerializeField] private int maxBullets = 60;
+    [SerializeField] private int currentBullets;
+    [SerializeField] private int bulletLoadFrequency = 1;
+    private bool magazineFull = true;
+    public static MagazineSize instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null || instance != this)
+        {
+            Destroy(this);
+        } else
+        {
+            instance = this;
+            currentBullets = maxBullets;
+        }
+        StartCoroutine(bulletLoad());
+    }
+
+    IEnumerator bulletLoad()
+    {
+        yield return new WaitForSeconds(2/bulletLoadFrequency);
+        if (!magazineFull)
+        {
+            currentBullets++;
+            if(currentBullets == maxBullets)
+            {
+                magazineFull = true;
+            } else
+            {
+                magazineFull = false;
+            }
+            StartCoroutine(bulletLoad());
+        } else
+        {
+            StartCoroutine(bulletLoad());
+        }
+    }
+
+    public bool canShoot(int bulletCount)
+    {
+        if(currentBullets-bulletCount > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
