77f4ce3dd8a49ecdd8ebc2b520f0c0176c216ccf
Fakan Bálint Dénes <fakan.balint@gmail.com>
2024-05-28 23:45:02 +0200

    guns added with the base moddel of gun

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public int damage = 1;
+    public bool shoulPierce = false;
     void Start()
     {
-        
         Destroy(gameObject, 3f);
     }
 
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent<IDamageable>(out IDamageable damageable)){
+            damageable.TakeDamage(damage);
+
+            if(!shoulPierce){
+                Destroy(gameObject);
+            }
+        }
+    }
 }
+
