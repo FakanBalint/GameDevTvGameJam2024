edd25faaaa2b6dcb6fa7f8bfb726a26055660176
Fakan Bálint Dénes <fakan.balint@gmail.com>
2024-05-31 18:06:15 +0200

    bullet + sprite shader

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,14 +6,23 @@ public class Bullet : MonoBehaviour
 {
     public int damage = 1;
     public bool shoulPierce = false;
+    public GameObject impactEffect;
     void Start()
     {
         Destroy(gameObject, 3f);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
+        
+
        if(other.TryGetComponent<IDamageable>(out IDamageable damageable)){
            damageable.TakeDamage(damage);
+                if(impactEffect != null){
+                GameObject o =  Instantiate(impactEffect, transform.position, Quaternion.identity);
+                o.transform.localScale *= damage;
+                Destroy(o, 0.25f);
+            }
+           
            if(!shoulPierce){
                Destroy(gameObject);
            }
